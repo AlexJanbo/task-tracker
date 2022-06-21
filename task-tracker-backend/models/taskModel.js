4fcc8081ca5a78a0ba5b3580285366a404bf52d1
AlexJanbo <Ajanbo@uci.edu>
2022-06-21 16:19:56 -0700

    Updated index.js and Routes Model and Conotroller for Users.

--- task-tracker-backend/models/taskModel.js
+++ task-tracker-backend/models/taskModel.js
@@ -2,8 +2,19 @@ import mongoose from 'mongoose'
 
 const taskSchema = mongoose.Schema(
     {
-        text: [
-            
-        ]
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+        Title: {
+            type: String,
+            required: [true, "Please add a text title"]
+        },
+        Description: {
+            type: String,
+
+        },
+        
     }
 )
