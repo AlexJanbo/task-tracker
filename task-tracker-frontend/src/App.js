e0da0676a2e1e459ffd7bed5fade1361c104a11e
AlexJanbo <Ajanbo@uci.edu>
2022-07-21 23:29:31 -0700

    Made stack only show up on homepage

--- task-tracker-frontend/src/App.js
+++ task-tracker-frontend/src/App.js
@@ -19,36 +19,6 @@ export default function App() {
                     <Route path='/signup' element={<Signup />}/>
                     <Route path='/dashboard' element={<Dashboard />}/>
                 </Routes>    
-                <Stack direction="column" spacing={2}>
-                    <Box
-                        height="400px"
-                        bgcolor="green"
-                    >
-                        <Card>
-
-                        </Card>
-                    </Box>
-                    <Box
-                        height="100px"
-                        bgcolor="darkgreen"
-                    >
-                    </Box>
-                    <Box
-                        height="400px"
-                        bgcolor="blue"
-                    >
-                    </Box>
-                    <Box
-                        height="400px"
-                        bgcolor="red"
-                    >
-                    </Box>
-                    <Box
-                        height="400px"
-                        bgcolor="purple"
-                    >
-                    </Box>
-                </Stack>
             </Router>
         </>
     )
