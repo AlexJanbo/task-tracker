9abad7c025ba81089b419adf6c4a8993f43a7e8e
AlexJanbo <Ajanbo@uci.edu>
2022-07-24 22:46:52 -0700

    Removed unused mui imports

--- task-tracker-frontend/src/App.js
+++ task-tracker-frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Box, Card, Stack } from '@mui/material'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom' 
 import Homepage from './pages/Homepage'
 import Dashboard from './pages/Dashboard'
