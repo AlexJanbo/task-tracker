7a1c4a19fc8a371ec158de9030da5b126d0dd49a
AlexJanbo <Ajanbo@uci.edu>
2022-07-28 03:47:36 -0700

    Added priority and status to my task form

--- task-tracker-backend/models/userModel.js
+++ task-tracker-backend/models/userModel.js
@@ -14,7 +14,7 @@ const userSchema = mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Please add a password']
-    }
+    },
 },
 {
     timestamps: true
--- task-tracker-frontend/src/app/store.js
+++ task-tracker-frontend/src/app/store.js
@@ -9,4 +9,5 @@ export const store = configureStore({
         auth: authReducer,
         tasks: taskReducer,
     }
+    
 })
