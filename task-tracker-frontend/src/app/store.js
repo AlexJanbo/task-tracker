42e429a32289fdd50974b86cb1c300dadb803d59
AlexJanbo <Ajanbo@uci.edu>
2022-08-25 11:31:43 -0700

    Setup table and form for user projects

--- task-tracker-frontend/src/app/store.js
+++ task-tracker-frontend/src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
 import taskReducer from '../features/tasks/taskSlice'
+import projectReducer from '../features/projects/projectSlice'
 
 
 
@@ -8,6 +9,7 @@ export const store = configureStore({
     reducer: {
         auth: authReducer,
         tasks: taskReducer,
+        projects: projectReducer,
     }
     
 })
