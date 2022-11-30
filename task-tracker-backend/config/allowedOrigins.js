123050d85af068dae4675edcc0789081c858f0bd
AlexJanbo <Ajanbo@uci.edu>
2022-11-30 09:11:10 -0800

    Trying cors settings

--- task-tracker-backend/config/allowedOrigins.js
+++ task-tracker-backend/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://task-tracker-project-alex.netlify.app/login'
+    'https://task-tracker-project-alex.netlify.app',
+    'https://task-tracker-api-rehs.onrender.com/'
 ]
 
 module.exports = allowedOrigins
--- task-tracker-backend/config/corsOptions.js
+++ task-tracker-backend/config/corsOptions.js
@@ -1,9 +1,9 @@
 const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
+    origin: (req, callback) => {
+        if (allowedOrigins.indexOf(req.header("Origin")) !== -1 || !origin) {
+            corsOptions = { origin: true }
         } else {
             callback(new Error('Not allowed by CORS'))
         }
