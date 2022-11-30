fdd7ef25b5e8b028f7091b67e36c25eb19dcd035
AlexJanbo <Ajanbo@uci.edu>
2022-11-29 17:39:49 -0800

    dev start back to index.js

--- task-tracker-backend/server.js
+++ task-tracker-backend/server.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 4000
-mongoose.connect(process.env.MONGODB_URI)
 
 const app = require("./index.js")
 
