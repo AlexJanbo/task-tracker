aadf31d392fe8dcf865f8e4ab81c90235a28ee5a
AlexJanbo <Ajanbo@uci.edu>
2022-11-29 16:41:56 -0800

    useNewUrlParser

--- task-tracker-backend/server.js
+++ task-tracker-backend/server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 4000
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
 
 const app = require("./index.js")
 
