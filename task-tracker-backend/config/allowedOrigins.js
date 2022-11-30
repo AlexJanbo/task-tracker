48224f73a58d9fac21fccd36989856debe9c5735
AlexJanbo <Ajanbo@uci.edu>
2022-11-30 09:47:22 -0800

    changed cors setting to work with local front end

--- task-tracker-backend/config/allowedOrigins.js
+++ task-tracker-backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'https://task-tracker-project-alex.netlify.app',
-    'https://task-tracker-api-rehs.onrender.com/'
+    'https://task-tracker-api-rehs.onrender.com/',
+    'https://localhost:3000'
 ]
 
 module.exports = allowedOrigins
