fc54e2a02e38ded4ef52773888746c8983d26f18
AlexJanbo <Ajanbo@uci.edu>
2022-11-29 17:44:16 -0800

    Changed dbConfig to not have newUrlParser

--- task-tracker-backend/config/db.js
+++ task-tracker-backend/config/db.js
@@ -4,7 +4,7 @@ const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_U
 
 const connectDB = async () => {
     try {
-        const con = await mongoose.connect(MONGODB_URI, { useNewUrlParser: true})
+        const con = await mongoose.connect(MONGODB_URI)
 
         console.log(`MongoDB successfully connected at:${con.connection.host}`.cyan)
     }
